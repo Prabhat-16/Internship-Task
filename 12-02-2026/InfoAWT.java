67a4bcfe4bdd91cc098a671b5dfff6773de9827f
Prabhat Mishra <mprabhat1607@gmail.com>
2026-02-13 21:23:12 +0530

    Added Landing and Portfolio

--- 12-02-2026/InfoAWT.java
+++ 12-02-2026/InfoAWT.java
@@ -8,31 +8,24 @@ public class InfoAWT extends Frame implements ActionListener {
 
     InfoAWT() {
 
-        // Set layout
         setLayout(new FlowLayout());
 
-        // Create buttons
         btnA = new Button("A");
         btnB = new Button("B");
 
-        // Create label to show result
         result = new Label("Press a button...");
 
-        // Add components
         add(btnA);
         add(btnB);
         add(result);
 
-        // Add action listener
         btnA.addActionListener(this);
         btnB.addActionListener(this);
 
-        // Frame settings
         setSize(400, 200);
         setTitle("AWT Button Example");
         setVisible(true);
 
-        // Close window properly
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 dispose();
@@ -40,7 +33,6 @@ public class InfoAWT extends Frame implements ActionListener {
         });
     }
 
-    // Event Handling
     public void actionPerformed(ActionEvent e) {
 
         if (e.getSource() == btnA) {
